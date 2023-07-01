344106d808cfcc3a3bdca52cf970199c6b1aabfe
dingduck <kfove.liang@gmail.com>
2023-07-01 21:21:42 +0800

    ....

--- examples/rtic-rs-app/examples/serial-dma.rs
+++ examples/rtic-rs-app/examples/serial-dma.rs
@@ -5,7 +5,6 @@ use cortex_m::singleton;
 use cortex_m_rt::entry;
 use panic_halt as _;
 use stm32f1xx_hal::{
-    device::can1::tx,
     pac,
     prelude::*,
     serial::{Config, Serial},
@@ -38,10 +37,18 @@ fn main() -> ! {
 
     let tx = serial.tx.with_dma(channels.2);
     let rx = serial.rx.with_dma(channels.3);
+    // cortex-m::singleton会创建一个静态对象，并返回Option<&'static mut T>
+    // 所以这东西不应该在loop中使用，这导致多个变量拥有可变所有权,在rtic中应该可以
     let buf = singleton!(: [u8; 8] = [0; 8]).unwrap();
 
-    // 这里做演示, rx,tx不能放入循环里面,read和write都会移走所有权，这很麻烦，具体后面会完善
-    let (buf, _rx) = rx.read(&mut *buf).wait();
-    let (_, _) = tx.write(buf.get(0).unwrap()).wait();
+    // .read() 和 .write() 都会丢弃获取所有权返回.如果在循环中,当第一次循环结束后,buf和rx或tx已经drop了，第二次是没有的
+    // 同一作用域下可以，请不要把他两放在循环中
+    // 这里问题是read和write接受的是mut T的参数,不是引用，而且是可变所有权,Rc行不通,Rc<RefCell>具有内部可变性，但是返回是Ref或RefMut
+    // 虽然实现了deref,但是还是不行啊
+    // 最终问题是，我怎么做资源分配，这两方法会把所有权转移，确实难搞，技术现在还不够
+    let (buf, rx) = rx.read(buf).wait();
+    let (buf, _rx) = rx.read(buf).wait();
+
+    let (_buf, _tx) = tx.write(buf).wait();
     loop {}
 }
